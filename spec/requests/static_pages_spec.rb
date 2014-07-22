8ecdc746c85250dd7544a2125659968e66f804bc
Amol Chavan <amolchavan0619@gmail.com>
2014-07-22 19:37:19 +0530

    add contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,12 +43,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact Us'" do
-      visit '/static_pages/Contact'
+      visit '/static_pages/contact'
       expect(page).to have_content('Contact Us')
     end
 
     it "should have the title 'Contact Us'" do
-      visit '/static_pages/about'
+      visit '/static_pages/contact'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
     end
   end
